6013ca6ab55c67a4c5a6db3b1daeb738ce50128e
정재호 <91364411+jjaeho0415@users.noreply.github.com>
2024-04-21 21:25:31 +0900

    Style: 스플래시 페이지 레이아웃 구현 완료 (#4)
    * Style: 스플래시 페이지 레이아웃 작업완료
    
    * Style:버튼 색깔 수정

---
+++ okivery/src/layout/Layout.tsx
@@ -0,0 +1,12 @@
+import "../styles/layout/Layout.css";
+import SplashPage from "../pages/SplashPage";
+
+const Layout: React.FC = () => {
+  return (
+    <div className='container'>
+      <SplashPage />
+    </div>
+  );
+};
+
+export default Layout;

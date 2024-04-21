9a097c7c5b4be8a5a80a38589b6d44628352954b
woic-ej <77326820+woic-ej@users.noreply.github.com>
2024-04-21 14:05:42 +0900

    Conf : Reorganizing directory structure (#1)

---
+++ okivery/.prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  arrowParens: "always",
+  htmlWhitespaceSensitivity: "css",
+  bracketSameLine: false,
+  printWidth: 80,
+  quoteProps: "as-needed",
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "es5",
+};

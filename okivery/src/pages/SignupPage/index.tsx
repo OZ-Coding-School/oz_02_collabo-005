f8a83051d9a2e9f7483dfa6993c99f42838d17b5
woic-ej <woic0102@naver.com>
2024-04-22 00:01:17 +0900

    Style : Button 스타일 수정, SignupPage에 Button 컴포넌트 적용

--- okivery/src/pages/SignupPage/index.tsx
+++ okivery/src/pages/SignupPage/index.tsx
@@ -1,37 +1,37 @@
+import Button from "../../components/common/button/Button";
 import Header from "../../components/common/header/Header";
 import BirthdayInputForm from "../../components/signup/BirthdayInputForm";
 import InputFormItem from "../../components/signup/InputFormItem";
-import Layout from "../../layout/Layout";
 import "../../styles/signup/page/SignupPage.css";
 
-const SignupPage = () => {
+const SignupPage: React.FC = () => {
   return (
     <>
-      <Layout>
-        <Header />
-        <div className="signup-container">
-          <h1 className="signup-title">Sign Up</h1>
-          <form>
-            <InputFormItem label="Name" name="name" type="text" />
-            <InputFormItem label="E-Mail" name="email" type="email" />
-            <InputFormItem label="Password" name="password" type="password" />
-            <InputFormItem
-              label="Repeat Password"
-              name="repeat-password"
-              type="password"
-            />
-            <InputFormItem label="Phone number" name="phone" type="number" />
-            <BirthdayInputForm />
-            <div className="term-container">
-              <label>
-                Terms <span>*</span>
-              </label>
-              <button className="term-btn">terms of service</button>
-            </div>
-            <button>Sign up</button>
-          </form>
-        </div>
-      </Layout>
+      <Header />
+      <div className="signup-container">
+        <h1 className="signup-title">Sign Up</h1>
+        <form>
+          <InputFormItem label="Name" name="name" type="text" />
+          <InputFormItem label="E-Mail" name="email" type="email" />
+          <InputFormItem label="Password" name="password" type="password" />
+          <InputFormItem
+            label="Repeat Password"
+            name="repeat-password"
+            type="password"
+          />
+          <InputFormItem label="Phone number" name="phone" type="number" />
+          <BirthdayInputForm />
+          <div className="term-container">
+            <label>
+              Terms <span>*</span>
+            </label>
+            <Button name="terms of service" backgroundColor="#FF6347" to="/" />
+          </div>
+          <div className="signup-btn">
+            <Button name="Sign up" backgroundColor="#FF6347" to="/login" />
+          </div>
+        </form>
+      </div>
     </>
   );
 };
--- okivery/src/pages/SplashPage/index.tsx
+++ okivery/src/pages/SplashPage/index.tsx
@@ -6,25 +6,25 @@ import Button from "../../components/common/button/Button";
 const index: React.FC = () => {
   return (
     <>
-      <div className='splashMainContainer'>
-        <div className='splashSubContainer'>
-          <div className='logoContainer'>
-            <img src={logoImage} width='128px' height='128px' />
+      <div className="splashMainContainer">
+        <div className="splashSubContainer">
+          <div className="logoContainer">
+            <img src={logoImage} width="128px" height="128px" />
           </div>
-          <div className='appNameTextContainer'>
-            <div className='appNameDescription'>
+          <div className="appNameTextContainer">
+            <div className="appNameDescription">
               <h1>
                 Okay + Free
                 <br />
                 Delivery =
               </h1>
             </div>
-            <div className='appName'>
+            <div className="appName">
               <h1>okivery</h1>
             </div>
           </div>
-          <div className='loginButton'>
-            <Button name='Login' backgroundColor='#414f42' to='/login' />
+          <div className="loginButton">
+            <Button name="Login" backgroundColor="#414f42" to="/login" />
           </div>
         </div>
       </div>

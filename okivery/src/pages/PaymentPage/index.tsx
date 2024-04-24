785bb37218e39ac9bf025fa3c07af371f37f3de0
jjaeho0415 <jjh0773@naver.com>
2024-04-24 16:38:55 +0900

    Style: PaymentPage 레이아웃 작업 완료

--- okivery/src/pages/PaymentPage/index.tsx
+++ okivery/src/pages/PaymentPage/index.tsx
@@ -1,7 +1,32 @@
 import React from "react";
+import Header from "../../components/common/header/Header";
+import CardManagementSection from "../../components/common/addcard/CardManagementSection";
+import "./PaymentPage.css";
+import Button from "../../components/common/button/Button";
 
 const index: React.FC = () => {
-  return <div>index</div>;
+  return (
+    <>
+      <Header
+        hasBackIcon={true}
+        to="/order/sheet"
+        title="Payment"
+        hasCartIcon={false}
+      />
+      <div className="paymentMainContainer">
+        <div className="cardContainer">
+          <CardManagementSection />
+        </div>
+        <div className="totalAccount">
+          <span className="totalText">Your Total:</span>
+          <span className="totalValue">41,600원</span>
+        </div>
+        <div className="payNowButtonSection">
+          <Button name="Pay now" backgroundColor="#FF6347" to="/order/status" />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default index;

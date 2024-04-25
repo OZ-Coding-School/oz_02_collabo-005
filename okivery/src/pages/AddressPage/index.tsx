d781f661ab3bf97efddda315336da7351715ab4d
jjaeho0415 <jjh0773@naver.com>
2024-04-25 15:30:05 +0900

    Style:paymentPage 요소들 위로 쏠림 현상 해결

--- okivery/src/pages/AddressPage/index.tsx
+++ okivery/src/pages/AddressPage/index.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import "./AddressPage.css";
+import Header from "../../components/common/header/Header";
 
 const AddressPage: React.FC = () => {
-  return <div>AddressPage</div>;
+  return (
+    <>
+      <Header hasBackIcon={true} to="/home" title="" hasCartIcon={false} />
+    </>
+  );
 };
 
 export default AddressPage;

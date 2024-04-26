37017c32234944a18c22b25a745e66fbd2cf2287
jjaeho0415 <jjh0773@naver.com>
2024-04-26 19:16:25 +0900

    Style: AddressPage 레이아웃 작업 완료

--- okivery/src/pages/AddressPage/index.tsx
+++ okivery/src/pages/AddressPage/index.tsx
@@ -2,16 +2,39 @@ import React, { useState } from "react";
 import "./AddressPage.css";
 import Header from "../../components/common/header/Header";
 import selectMapIcon from "../../assets/icons/selectMapIcon.png";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import mapSearchIcon from "../../assets/icons/searchIcon.png";
+import InputFormItem from "../../components/common/input/InputFormItem";
+import ServiceableMapImage from "../../assets/images/mapRadius.png";
+import Button from "../../components/common/button/Button";
+
+type AddressType = {
+  mainAddress: string;
+  subAddress: string;
+};
 
 const AddressPage: React.FC = () => {
-  const [addressData, setAddressData] = useState<string>("");
+  const navigate = useNavigate();
+  const initialAddressData: AddressType = {
+    mainAddress: "",
+    subAddress: "",
+  };
+  const [addressData, setAddressData] = useState(initialAddressData);
   const handleInputChange = (
     event: React.ChangeEvent<HTMLInputElement>
   ): void => {
-    const inputValue: string = event.target.value;
-    setAddressData(inputValue);
+    event.preventDefault();
+
+    const inputName = event.target.name;
+    const inputData = event.target.value;
+    setAddressData({
+      ...addressData,
+      [inputName]: inputData,
+    });
+  };
+
+  const handleSave = (): void => {
+    navigate(-1);
   };
 
   return (
@@ -21,28 +44,50 @@ const AddressPage: React.FC = () => {
         <div className="setAddressTextSection">
           <h2>Set delivery address</h2>
         </div>
-        <div className="setAddressContentSection">
+        <div>
           <div className="selectMapSection">
             <Link to="/address/select-map">
               <button>
-                <img
-                  src={selectMapIcon}
-                  style={{ width: "16px", height: "16px" }}
-                />
+                <img src={selectMapIcon} />
                 Find your location on the map
               </button>
             </Link>
             <div className="selectAddressTextInput">
               <img src={mapSearchIcon} />
-              <input
+              <InputFormItem
+                label=""
+                name="mainAddress"
                 type="text"
-                name="selectAddress"
-                id="selectAddress"
-                value={addressData}
-                onChange={handleInputChange}
-                placeholder="Type in your address"
-              ></input>
+                value={addressData.mainAddress}
+                handleInputChange={handleInputChange}
+                place="Type in your address"
+                isMust={false}
+                isEdit={true}
+              />
             </div>
+          </div>
+          <div className="detailAddress">
+            <InputFormItem
+              label="Delivery detail"
+              name="subAddress"
+              type="text"
+              value={addressData.subAddress}
+              place="Please enter the details address."
+              isMust={false}
+              handleInputChange={handleInputChange}
+              isEdit={true}
+            />
+          </div>
+          <div className="mapImageSection">
+            <div>Serviceable area</div>
+            <img src={ServiceableMapImage} />
+          </div>
+          <div className="saveAddressButton">
+            <Button
+              name="Save your address"
+              handleClick={handleSave}
+              buttonType="bigButton"
+            />
           </div>
         </div>
       </div>

669d15f9e32773634a5f0a995db31b3aed3ce7c4
woic-ej <woic0102@naver.com>
2024-04-29 17:03:53 +0900

    Chore: 변경사항 하나 더 추가해서 머지함

--- okivery/src/pages/ForeignCardPage/index.tsx
+++ okivery/src/pages/ForeignCardPage/index.tsx
@@ -50,81 +50,6 @@ const ForeignCardPage: React.FC = () => {
     <>
       <Header hasBackIcon={true} title="Register Card" hasCartIcon={false} />
       <div className="cardMainContainer">
-<<<<<<< HEAD
-        <div className="cardSubContainer">
-          <div className="cardButtonSection">
-            <h2>Foreign credit card</h2>
-            <SmallButton name="Save" handleClick={handleSave} />
-          </div>
-          <form>
-            <InputItem
-              label="Card Number"
-              name="cardNumber"
-              type="number"
-              isMust={true}
-              value={cardData.cardNumber}
-              isEdit={true}
-              place="Enter 16-digit card number"
-              handleInputChange={handleInputChange}
-            />
-            Expiration Date <span style={{ color: "red" }}>*</span>
-            <div className="expirationDateInputSection">
-              <label htmlFor="expirationDateMonth">
-                <input
-                  type="number"
-                  name="expirationDateMonth"
-                  id="expirationDateMonth"
-                  value={cardData.expirationDateMonth}
-                  placeholder="MM"
-                  onChange={handleInputChange}
-                  onInput={handleOnInput}
-                />
-              </label>
-              <label htmlFor="expirationDateYear">
-                <input
-                  type="number"
-                  name="expirationDateYear"
-                  id="expirationDateYear"
-                  value={cardData.expirationDateYear}
-                  placeholder="YY"
-                  onChange={handleInputChange}
-                  onInput={handleOnInput}
-                />
-              </label>
-            </div>
-            <InputItem
-              label="Cardholder Name"
-              name="cardholderName"
-              type="text"
-              isMust={true}
-              value={cardData.cardholderName}
-              isEdit={true}
-              place=""
-              handleInputChange={handleInputChange}
-            />
-            <InputItem
-              label="CVV/CVC"
-              name="cvv"
-              type="number"
-              isMust={true}
-              value={cardData.cvv}
-              isEdit={true}
-              place=""
-              handleInputChange={handleInputChange}
-            />
-            <InputItem
-              label="Pin number"
-              name="pinNumber"
-              type="number"
-              isMust={true}
-              value={cardData.pinNumber}
-              isEdit={true}
-              place="Enter in the first two pins here"
-              handleInputChange={handleInputChange}
-            />
-            <BirthdayInput isEdit={true} isMust={true} />
-          </form>
-=======
         <div className="cardButtonSection">
           <h2>Foreign credit card</h2>
           <Button
@@ -132,7 +57,6 @@ const ForeignCardPage: React.FC = () => {
             handleClick={handleSave}
             buttonType="smallButton"
           />
->>>>>>> 9057d0b31e4e285743c809e3e75c39f8cffe5fca
         </div>
         <form>
           <InputItem

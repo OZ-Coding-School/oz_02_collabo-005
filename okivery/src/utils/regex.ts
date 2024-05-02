445cf95e0d8f4198caf140a3630acf2060245ccb
woic-ej <woic0102@naver.com>
2024-05-02 17:49:24 +0900

    Feature: input 폼 유효성 검사

---
+++ okivery/src/utils/regex.ts
@@ -0,0 +1,6 @@
+export const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+export const passwordRegex =
+  /^(?=.*[a-zA-Z0-9])[a-zA-Z0-9!@#$%^&*()_+{}|:"<>?`\-=[\]\\;',./]{8,}$/;
+
+export const phoneRegex = /^010\d{8}$/;

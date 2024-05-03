895a759f09dcbd92f6c925b42dcfb802f7fc579f
woic-ej <woic0102@naver.com>
2024-05-03 14:23:05 +0900

    Style: OrdersPage 레이아웃 변경 및 OrderDetailsPage를 추가함

--- okivery/src/components/common/button/QuantityButton.tsx
+++ okivery/src/components/common/button/QuantityButton.tsx
@@ -5,20 +5,30 @@ interface QuantityButtonProps {
   quantity: number;
   handlePlusBtnClick: () => void;
   handleMinusBtnClick: () => void;
+  disabled?: boolean;
 }
 
 const QuantityButton: React.FC<QuantityButtonProps> = ({
   quantity,
   handlePlusBtnClick,
   handleMinusBtnClick,
+  disabled,
 }) => {
   return (
     <div className="quantityButtonContainer">
-      <button className="minus Btns" onClick={handleMinusBtnClick}>
+      <button
+        className="minus Btns"
+        onClick={handleMinusBtnClick}
+        disabled={disabled}
+      >
         -
       </button>
       <div className="menuQuantity"> {quantity} </div>
-      <button className="plus Btns" onClick={handlePlusBtnClick}>
+      <button
+        className="plus Btns"
+        onClick={handlePlusBtnClick}
+        disabled={disabled}
+      >
         +
       </button>
     </div>

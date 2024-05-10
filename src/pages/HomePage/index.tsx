b32b25b0ef2751dede59a21357cf6b3df9a7c6f0
woic-ej <77326820+woic-ej@users.noreply.github.com>
2024-05-10 15:44:48 +0900

    Feature/home: HomePage에서 API연동 (#68)
    * Feature: HomePage API 연동하여 레스토랑 리스트 받아옴, 카테고리별로 분류 완료
    
    * Style: RestaurantCategory 컴포넌트 스타일 및 오타 수정
    
    * Style: RestaurantItem 컴포넌트 스타일 수정

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,34 +1,72 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "@components/common/header/Header";
 import FooterNavigationBar from "@components/common/footer/FooterNavigationBar";
 import RestaurantCategory from "@components/home/category/RestaurantCategory";
 import Banner from "@components/home/banner/Banner";
 import AddressBar from "@components/address/AddressBar";
+import apiRoutes from "../../api/apiRoutes";
+import customAxios from "./../../api/axios";
 import "./HomePage.css";
-
-export type Restaurant = {
-  name: string;
-  intro: string;
-};
+import { RestaurantType } from "../../types/types";
 
 export type BannerType = {
   name: string;
 };
 
 const HomePage: React.FC = () => {
-  const restaurants: Restaurant[] = [
-    { name: "가게1", intro: "설명 1" },
-    { name: "가게2", intro: "설명 2" },
-    { name: "가게3", intro: "설명 3" },
-    { name: "가게4", intro: "설명 4" },
-    { name: "가게5", intro: "설명 5" },
-  ];
-
   const banners: BannerType[] = [
     { name: "banner1" },
     { name: "banner2" },
     { name: "banner3" },
   ];
+
+  const [restaurants, setRestaurants] = useState<RestaurantType[]>();
+
+  const [categories, setCategories] = useState<{
+    [key: string]: RestaurantType[];
+  }>({});
+
+  useEffect(() => {
+    const fetchRestaurants = async () => {
+      try {
+        const response = await customAxios.get(apiRoutes.restaurantList);
+        if (response.status !== 200) throw new Error("예외가 발생했습니다.");
+        setRestaurants(response.data);
+      } catch (error) {
+        console.error("Failed to fetch restaurants:", error);
+      }
+    };
+    fetchRestaurants();
+  }, []);
+
+  useEffect(() => {
+    const extractCategories = () => {
+      if (!restaurants || restaurants.length === 0) {
+        setCategories({});
+        return;
+      }
+
+      const categoriesSet = new Set();
+
+      restaurants.forEach((restaurant) => {
+        restaurant.category.forEach((category) => {
+          categoriesSet.add(category);
+        });
+      });
+
+      const updatedCategories: { [key: string]: RestaurantType[] } = {};
+
+      Array.from(categoriesSet).forEach((category) => {
+        const categoryRes = restaurants.filter((restaurant) =>
+          restaurant.category.includes(category)
+        );
+        updatedCategories[category] = categoryRes;
+      });
+      setCategories(updatedCategories);
+    };
+    extractCategories();
+  }, [restaurants]);
+
   return (
     <div>
       <Header
@@ -41,21 +79,13 @@ const HomePage: React.FC = () => {
       <main className="mainContainer">
         <Banner banners={banners} />
         <div className="categoryList">
-          <RestaurantCategory
-            title="American & Grills"
-            id="AG"
-            restaurants={restaurants}
-          />
-          <RestaurantCategory
-            title="Korean & Fusion"
-            id="KF"
-            restaurants={restaurants}
-          />
-          <RestaurantCategory
-            title="Dessert & Drinks"
-            id="DD"
-            restaurants={restaurants}
-          />
+          {Object.keys(categories).map((categoryKey) => (
+            <RestaurantCategory
+              key={categoryKey}
+              title={categoryKey}
+              restaurants={categories[categoryKey]}
+            />
+          ))}
         </div>
       </main>
       <FooterNavigationBar page="Home" />

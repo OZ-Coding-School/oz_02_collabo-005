f0ee84e6c2432c680342bd9b035ca6c7567fcff7
jjaeho0415 <jjh0773@naver.com>
2024-05-13 20:29:16 +0900

    Chore: 구글 맵 모달의 구글 지도에 그릴 다각형의 경계를 위한 경도와 위도, 다각형 그리는 스타일을 파일로 생성 후 저장

---
+++ src/components/address/PolygonStyle.ts
@@ -0,0 +1,10 @@
+import freeDeliveryFeePolygonCoords from "../../constants/freeDelivery";
+
+export const freeDeliveryPolygonOptions = {
+  paths: freeDeliveryFeePolygonCoords,
+  strokeColor: "#FF0000", // 테두리 색상
+  strokeOpacity: 0.8, // 테두리 투명도
+  strokeWeight: 2, // 테두리 두께
+  fillColor: "#FF0000", // 내부 색상
+  fillOpacity: 0.35, // 내부 투명도
+};

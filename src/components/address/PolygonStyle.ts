e85fb3ad54728d9cab0fab756d1b03bcee07bd92
jjaeho0415 <jjh0773@naver.com>
2024-05-13 22:35:20 +0900

    Chore: 배달 서비스 이용가능한 구역의 위도 경도 데이터 저장 후 polygon으로 구역 표시

--- src/components/address/PolygonStyle.ts
+++ src/components/address/PolygonStyle.ts
@@ -1,4 +1,7 @@
-import freeDeliveryFeePolygonCoords from "../../constants/freeDelivery";
+import {
+  freeDeliveryFeePolygonCoords,
+  serviceablePolygonCoords,
+} from "../../constants/polygonLocation";
 
 export const freeDeliveryPolygonOptions = {
   paths: freeDeliveryFeePolygonCoords,
@@ -6,5 +9,14 @@ export const freeDeliveryPolygonOptions = {
   strokeOpacity: 0.8, // 테두리 투명도
   strokeWeight: 2, // 테두리 두께
   fillColor: "#FF0000", // 내부 색상
+  fillOpacity: 0.35, // 내부 투명도
+};
+
+export const serviceablePolygonOptions = {
+  paths: serviceablePolygonCoords,
+  strokeColor: "#00FF00", // 테두리 색상
+  strokeOpacity: 0.8, // 테두리 투명도
+  strokeWeight: 2, // 테두리 두께
+  fillColor: "#00FF00", // 내부 색상
   fillOpacity: 0.35, // 내부 투명도
 };

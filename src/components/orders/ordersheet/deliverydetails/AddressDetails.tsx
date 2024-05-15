ee3c2cb322177aa70b51301c9d04552ba2753658
정재호 <91364411+jjaeho0415@users.noreply.github.com>
2024-05-16 00:16:06 +0900

    Refactor: OrderDetails 페이지 하드코딩 되어있던 부분 가져올 데이터에 맞게 타입 지정해서 수정하고 AddressDetails 컴포넌트 수정 (#80)

--- src/components/orders/ordersheet/deliverydetails/AddressDetails.tsx
+++ src/components/orders/ordersheet/deliverydetails/AddressDetails.tsx
@@ -1,16 +1,17 @@
 import React from "react";
 import "./AddressDetails.css";
+import { AddressType } from "../../../../types/addressType";
 
-const AddressDetails: React.FC = () => {
+const AddressDetails: React.FC<AddressType> = ({ mainAddress, subAddress }) => {
   return (
     <div className="addressDetailsContainer">
       <div>
         <div className="addressDetailsTitle">Address</div>
-        <div>Bay Area, San Francisco, California, USA</div>
+        <div>{mainAddress}</div>
       </div>
       <div>
         <div className="addressDetailsTitle">Detailed address</div>
-        <div>Building 103, Room 1203</div>
+        <div>{subAddress}</div>
       </div>
     </div>
   );

c67ac4b127ac338b4fdab4930c04f384860f028e
jjaeho0415 <jjh0773@naver.com>
2024-05-17 16:41:55 +0900

    Refactor: 주문서 페이지의 주소부분 props하나로 바꿈

--- src/components/orders/ordersheet/deliverydetails/AddressDetails.tsx
+++ src/components/orders/ordersheet/deliverydetails/AddressDetails.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import "./AddressDetails.css";
-import { AddressType } from "../../../../types/addressType";
 
-const AddressDetails: React.FC<AddressType> = ({ mainAddress, subAddress }) => {
+interface addressDetails {
+  addressData: string;
+}
+
+const AddressDetails: React.FC<addressDetails> = ({ addressData }) => {
   return (
     <div className="addressDetailsContainer">
       <div>
-        <div className="addressDetails">{`${mainAddress} ${subAddress}`}</div>
+        <div className="addressDetails">{addressData}</div>
       </div>
     </div>
   );

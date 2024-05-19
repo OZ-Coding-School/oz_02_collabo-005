0e61f3a03c71fc9034b89c3a76ce3b78bbfe7e27
woic-ej <woic0102@naver.com>
2024-05-20 02:02:41 +0900

    Feature : CS Page 로딩중 구현

--- src/pages/CustomerServicePage/index.tsx
+++ src/pages/CustomerServicePage/index.tsx
@@ -1,10 +1,17 @@
 import Header from "@components/common/header/Header";
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./CustomerServicePage.css";
+import Loading from "@components/common/loading/loading";
 
 const CustomerServicePage: React.FC = () => {
   const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(true);
+
+  // `onLoad` 이벤트 핸들러 함수
+  const handleIframeLoad = () => {
+    setIsLoading(false);
+  };
 
   return (
     <>
@@ -14,10 +21,12 @@ const CustomerServicePage: React.FC = () => {
         hasCartIcon={false}
         handleBackIconClick={() => navigate("/home")}
       />
-      <div className="CScontainer">
+      {isLoading && <Loading />}
+      <div className={`CScontainer ${isLoading ? "loading" : ""}`}>
         <iframe
           src="https://docs.google.com/forms/d/e/1FAIpQLSccQRUNJWp41IlopstJiL28sfcpAPebBNImuhAtQmU1H6HTuQ/viewform"
           className="CSiframe"
+          onLoad={handleIframeLoad}
         />
       </div>
     </>

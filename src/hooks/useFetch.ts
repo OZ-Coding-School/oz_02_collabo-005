c8504cd8fd025515a1d7a1a8c95981af75d27811
ludacirs <ludacirs@naver.com>
2024-05-21 22:19:38 +0900

    API요청 추상화

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,23 +1,35 @@
-import { useEffect, useState } from "react";
-import customAxios from "../api/axios";
+import { useEffect, useState } from 'react';
+import customAxios from '../api/axios';
 
-const useGet = (url: string) => {
-  const [payload, setPayload] = useState(null);
+// get
+const useQuery = <T>(url: string) => {
+  const [data, setData] = useState<T | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [_, forceRender] = useState(0);
+
+  const refetch = () => {
+    forceRender((prev) => prev + 1);
+    return data;
+  };
 
   useEffect(() => {
     const callUrl = async () => {
+      setIsLoading(true);
       try {
-        const { data } = await customAxios.get(url);
-        setPayload(data);
+        const { data } = await customAxios.get<T>(url);
+        setData(data);
       } catch (error) {
         console.error(error);
+      } finally {
+        setIsLoading(false);
       }
     };
 
     callUrl();
   }, [url]); // url이 변경될 때만 호출
 
-  return payload;
+  return { data, isLoading, refetch };
 };
+// post, put, patch, delete
 
-export default useGet;
+export { useQuery };
